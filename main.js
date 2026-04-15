4e2d9bd34289f333dfd22d0764f16b91e266e93e
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-04-15 10:24:19 +0300

    feat: add project files

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ const API_KEY = `https://api.dictionaryapi.dev/api/v2/entries/en/<word>`;
 
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
-const searchContainer = document.getElementById("search-result");
+const resultCard = document.getElementById("result-card");
+const savedContainer = document.getElementById("fav-panel");
+const searchContainer = document.getElementById("search-panel");
+const favContainer = document.getElementById("fav-panel");
+
+searchInput.addEventListener("keydown", e => {
+    if (e.key === "Enter") searchWord;
+});
 
 async function searchWord() {
-    const word = document.getElementById("search-input").value.trim();
+    const query = searchInput.value.trim();
+
+    if (!query) {
+        showError("Please enter a word.")
+    }
+
+    clearError();
+    resultContainer.innerHTML = "";
+
+    try {
+        
+    } catch (error) {
+        
+    }
 }
